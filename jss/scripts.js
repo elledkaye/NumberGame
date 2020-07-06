0659b0e59dd30042e878eadf3b7a65ea1e668d55
Danielle Kaye <41302248+elledkaye@users.noreply.github.com>
2020-07-06 12:58:26 -0400

    7/2/2020
    style changes
    js changes

--- jss/scripts.js
+++ jss/scripts.js
@@ -1,17 +1,21 @@
-var timeLimit, timerStart, currentDot;
+var timeLimit, timerStart, currentDot, progress;
 var timer = document.getElementById('timer');
+var progressBar = document.getElementById('gameBar');
 var playingField = document.getElementById('playingField');
+var levelNum = document.getElementById('levelNum'); 
+var btnPlay = document.getElementById('playButton');  
 const maxTimeLimit = 10;
 const initialState = "Start";
-const restartState = "Quit";
+const restartState = "Try Again";
 const second_interval = 1000;
-const dotsPerLevel = 10;
+const dotsPerLevel = 10; 
 
-function play(playButton){
+function play(_btnPlay){
     if(playButton.innerHTML === initialState){
         playButton.innerHTML = restartState;
         timeLimit = maxTimeLimit;
         currentDot = 1;
+        setLevelNum();
         removeDots();
         addDots();
         timerStart = setInterval(time, second_interval);
@@ -22,21 +26,59 @@ function play(playButton){
         removeDots();
         setTimeLimit();
     }
+
 }
 
+function timeStop(){
+    if(currentDot === dotsPerLevel && timer.innerHTML !== "0"){
+        clearInterval(timer);
+
+    }
+}
 function time(){
     timeLimit--;
-    if(timeLimit >= 0){
+    if(timeLimit >= 0){ 
+        //update the progress bar 
+       /* $("gameBarProgress").css('width',(progress+'%')); 
+        if(progress <= 66 && progress > 33){
+            $("gameBarProgress").removeClass('bg-success');
+            $("gameBarProgress").addClass('bg-warning');
+        }
+        else if(progress <= 33){
+            $("gameBarProgress").removeClass('bg-warning');
+            $("gameBarProgress").addClass('bg-danger');
+ 
+        } 
+        else if(progress === 100){
+            $("gameBarProgress").removeClass('bg-warning');
+            $("gameBarProgress").removeClass('bg-danger');
+            $("gameBarProgress  ").addClass('bg-success');
+        } 
+        progress = progress - maxTimeLimit;*/
         timer.innerHTML = ":" + timeLimit;
-    }
+    }  
     else{
-        alert("You Lost!");
-        clearInterval(timerStart);
+        if(currentDot === dotsPerLevel && timer.innerHTML !== "0" )
+            alert("You Won!");
+            clearInterval(time, second_interval);
+        currentDot++;  
     }
+    
+    if(currentDot < dotsPerLevel && timer.innerHTML =="0"){ 
+        alert("You Lost!"); 
+        clearInterval(time, timer);
+        removeDots(); 
+    }        
+    
 }
 
 function setTimeLimit(){
     timer.innerHTML = ":" + maxTimeLimit;
+}
+
+function setLevelNum(){
+    levelNum.innerHTML = "1";
+    /*if user wins increment level #*/
 }
 
 function addDots(){
@@ -64,11 +106,15 @@ function verify(element){
         element.srcElement.className = "correct-dot";
         if(currentDot === dotsPerLevel && timer.innerHTML !== "0" )
             alert("You Won!");
+            clearInterval(time, second_interval);
         currentDot++;
+        /*if user wins stop timer*/
     }
     else
     {
         if(element.srcElement.className !== "correct-dot")
             element.srcElement.className = "incorrect-dot";
     }
+
+
 }
